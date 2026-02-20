74e62d164422393248611fc0b3732516179050ea
Saurabh Kumar <saurabh.maurya@buddyloan.com>
2026-02-20 17:04:09 +0530

    change lib jdm editor

--- libs/jdm-editor/packages/jdm-editor/src/components/decision-graph/index.ts
+++ libs/jdm-editor/packages/jdm-editor/src/components/decision-graph/index.ts
@@ -24,6 +24,7 @@ export {
   useEdgeDiff,
   NodeTypeKind,
 } from './context/dg-store.context';
+export type { ViewConfig, ViewConfigPermission } from './context/dg-store.context';
 export { NodeColor } from './nodes/specifications/colors';
 
 export type {
