2e5707f3a17cd90ba0d8e9622b27392b898be79a
Saurabh Kumar <saurabh.maurya@buddyloan.com>
2026-03-10 01:02:05 +0530

    change

--- apps/backend/ruleenginebe/src/app.ts
+++ apps/backend/ruleenginebe/src/app.ts
@@ -20,7 +20,7 @@ app.use(
 
 app.use(express.json({ limit: "10mb" }));
 app.use(express.urlencoded({ limit: "10mb", extended: true }));
-// app.use("/api/v1/logs", requestLoggerRoutes());
+app.use("/api/v1/logs", requestLoggerRoutes());
 app.use(requestLoggerMiddleware({ appName: "ruleenginebe" }));
 app.use(apiRouter);
 
