841ed54891de4dbd4e4cc06ae46ec0ea7ba15037
Saurabh Kumar <saurabh.maurya@buddyloan.com>
2026-03-23 10:30:21 +0530

    refactor: health check api added in rulenginebe

--- apps/backend/ruleenginebe/src/app.ts
+++ apps/backend/ruleenginebe/src/app.ts
@@ -17,6 +17,10 @@ app.use(
   })
 );
 
+app.get("/health", (req, res) => {
+  res.status(200).json({ message: "OK" });
+});
+
 app.use(express.json({ limit: "50mb" }));
 app.use(express.urlencoded({ limit: "50mb", extended: true }));
 app.use("/api/v1/logs", requestLoggerRoutes());
